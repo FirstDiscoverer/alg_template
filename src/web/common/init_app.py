c3d898570ddfab0eaa37e22f7cb33000d13ef4eb
青灯黄卷 <21698414+FirstDiscoverer@users.noreply.github.com>
2025-11-28 23:44:39 +0800

    feat: 异常兜底

--- src/web/common/init_app.py
+++ src/web/common/init_app.py
@@ -1,3 +1,5 @@
+import traceback
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
 from loguru import logger
@@ -12,6 +14,14 @@ def register_exception_handlers(app: FastAPI):
     async def my_exception_handler(request: Request, exc: MyException):
         logger.warning(f"【自定义异常】exception={exc}")
         return JSONResponse(Result.fail(msg=exc.msg, code=exc.code).model_dump())
+
+    @app.exception_handler(Exception)
+    async def unhandled_exception_handler(request: Request, exc: Exception):
+        # 建议打印 traceback，方便定位
+        tb = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__))
+        msg = f"【未捕获异常】path={request.url.path} exception={exc}\n{tb}"
+        logger.error(msg)
+        return JSONResponse(status_code=500, content=Result.fail(msg=msg, code="UNKNOW").model_dump())
 
     logger.info("register exception handlers success")
 
