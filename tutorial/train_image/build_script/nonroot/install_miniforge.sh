7db6f0c17a8df19c9fd60438c5ffc891cad038d9
青灯黄卷 <21698414+FirstDiscoverer@users.noreply.github.com>
2026-08-25 20:45:30 +0800

    feat: 源优化

--- tutorial/train_image/build_script/nonroot/install_miniforge.sh
+++ tutorial/train_image/build_script/nonroot/install_miniforge.sh
@@ -50,7 +50,7 @@ conda clean --all --yes --verbose
 
 # pip配置
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
-pip config set global.extra-index-url "https://mirrors.aliyun.com/pypi/simple https://mirrors.cloud.tencent.com/pypi/simple https://pypi.tuna.tsinghua.edu.cn/simple https://mirrors.bfsu.edu.cn/pypi/web/simple"
+pip config set global.extra-index-url "https://mirrors.aliyun.com/pypi/simple https://mirrors.cloud.tencent.com/pypi/simple https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple https://mirrors.bfsu.edu.cn/pypi/web/simple"
 
 # pip config set global.no-cache-dir true
 
