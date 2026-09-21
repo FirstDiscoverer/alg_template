f99d117c7a6358e0d76cdae38768a3a295453a4f
青灯黄卷 <21698414+FirstDiscoverer@users.noreply.github.com>
2026-09-21 17:28:21 +0800

    feat: uv

--- deploy/init_requirements.sh
+++ deploy/init_requirements.sh
@@ -9,10 +9,8 @@ TIMEOUT=30
 
 echo "是否使用UV: ${ENABLE_UV}"
 if [ "${ENABLE_UV}" != "false" ]; then
-  pip install uv
-
   export UV_HTTP_RETRIES=${RETRIES} UV_HTTP_TIMEOUT=${TIMEOUT}
-  uv pip install -r ${REQ_PATH}
+  uv pip install --system -r ${REQ_PATH}
 
   uv cache clean
 else
